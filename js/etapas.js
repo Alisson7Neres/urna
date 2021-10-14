f84b7f45cfc6f291252979ef4b82f35f1993707a
Alisson7Neres <alisson.neres@hotmail.com>
2021-10-14 11:38:04 -0300

    Projeto finalizado

---
+++ js/etapas.js
@@ -0,0 +1,58 @@
+let etapas = [
+    {
+        titulo: "VEREADOR",
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '12345',
+                nome: "Kernel",
+                partido: "PA",
+                fotos: [
+                    { url: '12345.png', legendas: 'Vereador' }
+                ]
+            },
+            {
+                numero: '54321',
+                nome: "Shell",
+                partido: "PRE",
+                fotos: [
+                    { url: '54321.png', legendas: 'Vereador' }
+                ]
+            },
+        ]
+    },
+    {
+        titulo: "PREFEITO",
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '33',
+                nome: "Kernel Transparente",
+                partido: "RA",
+                fotos: [
+                    { url: '33_1.png', legendas: 'Prefeito' },
+                    { url: '88.png', legendas: 'Vice-Prefeito', small: true }
+                    
+                ]
+            },
+            {
+                numero: '38',
+                nome: "Uni",
+                partido: "PRE",
+                fotos: [
+                    { url: '38_8.png', legendas: 'Prefeito' },
+                    { url: '84.png', legendas: 'Vice-Prefeito', small: true }
+                ]
+            },
+            {
+                numero: '34',
+                nome: "Kernel Ant",
+                partido: "PA",
+                fotos: [
+                    { url: '34_5.png', legendas: 'Prefeito' },
+                    { url: '54.png', legendas: 'Vice-Prefeito', small: true }
+                ]
+            },
+        ]
+    }
+];
